8427c21f5edea3c32b572f26fd01dd43f7c8b2c3
santiago sanchez chalarca <santiagoschalarca@gmail.com>
2026-02-20 11:58:30 -0500

    Agregar archivos de proyecto.

---
+++ senaConsole/Hotel/casoHotel.cs
@@ -0,0 +1,120 @@
+﻿using System.Numerics;
+
+namespace senaConsole.Hotel
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+    }
+
+    internal class CasoHotel
+    {
+        public static void CalculosHotel()
+        {
+            double individual = 2500;
+            double doble = 4600;
+            double familiar = 5200;
+            double iva = 0.16;
+            double totalPagar = 0;
+            double ganancias = 0;
+            double ivaG = 0;
+
+            Console.WriteLine("************** Bienbenidos al Hotel Anarecogete **************\n");
+
+            Console.Write("Cuantas personas se van a hospedar: ");
+            int huespedes = int.TryParse(Console.ReadLine(), out huespedes) && huespedes > 0 ? huespedes : 1;
+
+
+            while (huespedes > 0)
+            {
+            List<Persona> personas = new List<Persona>();
+                for (int i = 0; i < huespedes; i++)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine($"Persona # {i + 1}");
+                    Persona nuevaPersona = new Persona();
+                    Console.Write("Nombre: ");
+                    nuevaPersona.Nombre = Console.ReadLine() ?? "";
+
+                    Console.Write("Edad: ");
+                    nuevaPersona.Edad = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Sexo (M/F): ");
+                    nuevaPersona.Sexo = Console.ReadLine() ?? "";
+
+                    // qui se agrega a la lista la persona 
+
+                    personas.Add(nuevaPersona);
+
+
+                }
+                Console.Write("Cuantos dias se van a hospedar:  ");
+                int dias = int.TryParse(Console.ReadLine(), out dias) && dias > 0 ? dias : 1;
+
+
+                double tarifa;
+                double descuento;
+
+                if (huespedes >= 3)
+                {
+                    tarifa = familiar;
+                    descuento = 0.15;
+                }
+                else if (huespedes >= 2)
+                {
+                    tarifa = doble;
+                    descuento = 0.09;
+                }
+                else
+                {
+                    tarifa = individual;
+                    descuento = 0.05;
+                }
+
+                double total = dias * tarifa;
+                double totalConIva = total * iva + total;
+                totalPagar = totalConIva - (totalConIva * descuento);
+
+                // Acumular en `ganancias` inmediatamente después de calcular `totalPagar`
+                ganancias += totalPagar;
+                ivaG += totalConIva - total;
+
+                // Mostrar datos
+                Console.WriteLine("\n--- LISTA DE PERSONAS ---");
+
+                int contador = 1;
+                foreach (var persona in personas)
+                {
+                    Console.WriteLine($"\n" +
+                        $"Persona # {contador}\n" +
+                        $"Nombre: {persona.Nombre}\n" +
+                        $"Edad: {persona.Edad}\n" +
+                        $"Sexo: {persona.Sexo}");
+                    contador++;
+                }
+
+
+                Console.WriteLine("\n********************* Tarifas *********************");
+
+                Console.WriteLine($"\nSu precio sin iva es de {total}\n" +
+                $"su total con iva es {totalConIva} \n" +
+                $"y su total a pagar es de {totalPagar}");
+                Console.WriteLine("\n********************* Fin *********************\n");
+
+                Console.Write("Digite cuantas personas se van a hospedar o presione 0 para salir\n");
+
+                Console.Write("Cuantas personas se van a hospedar: ");
+                if (!int.TryParse(Console.ReadLine(), out huespedes) || huespedes == 0)
+                    break;
+            }
+
+            // Ahora `ganancias` contiene la suma de todos los `totalPagar` acumulados
+            // Puede mostrarse o usarse según sea necesario:
+            Console.WriteLine($"\nGanancias acumuladas: {ganancias}");
+            Console.WriteLine($"el total de iva a entregar {ivaG}");
+        }
+    }
+
+}
---
+++ senaConsole/Program.cs
@@ -0,0 +1,121 @@
+﻿using senaConsole.Hotel;
+
+namespace senaConsole
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CasoHotel.CalculosHotel();
+        }
+
+        public static void Listas() //************************ listas ***************************
+        {
+            string[] nombres = { "Ana", "Maria", "Jose", "Andres" };
+            foreach (var nombre in nombres)
+            {
+                Console.WriteLine(nombre.ToUpper());
+            }
+        }
+
+        public static void LlenarListas() //************************ llenar listas ***************************
+        {
+            // ************************ lista string ***************************
+            List<string> apellidos = new List<string>();
+            //la calse  objeto       crea   constructor
+
+            // ************************ llenar lista *****************************
+            apellidos.Capacity = 5; // caracteristicas , en programacion propiedad o
+                                    // atributo "no tiene ()" cumplen la funcion de una variabe , se le puede asignar un valor
+
+            // ***************************************** Diferencia ****************************************
+
+            //apellidos.Add(); ************** // acciones en programacion , metodos o funciones tiene "()"
+            apellidos.Add("Sanchez");
+            apellidos.Add("Chalarca");
+            apellidos.Add("Rodrigues");
+
+            foreach (var apellido in apellidos)
+            {
+                Console.WriteLine(apellido);
+            }
+            //************************ lista int ***************************
+            List<int> anios = new List<int>();
+            //la calse  objeto  crea   constructor 
+
+            anios.Capacity = 5;
+            anios.Add(2021);
+            anios.Add(2022);
+            anios.Add(2023);
+            anios.Add(2024);
+
+            foreach (var anio in anios)
+            {
+
+                Console.WriteLine(anio);
+            }
+        }
+
+        public static void Diccionario() //************************ diccionario ***************************
+        {
+            // Diccionario sintaxis
+            /* Dictionary<tipo de dato key , value > nombre_diccionario = new Dictionary <tipo datp key , value> ()*/
+            //{
+            //    { "key " , "value "},
+            //    { "key " , "value "},
+            //    { "key " , "value "},
+            //    { "key " , "value "}
+            //};
+            Dictionary<int, string> frutas = new Dictionary<int, string>()
+            {
+                { 1, "cebolla"},
+                { 2, "mango"},
+                { 3, "mamoncillo"},
+                { 4, "recogete"},
+            };
+            foreach (var fruta in frutas)
+            {
+                Console.WriteLine($"{fruta.Key} = {fruta.Value}");
+            }
+        }
+        public static void RegistroProductos() // aplicamos todo lo aprendido en este ejemplo 
+        {
+            Dictionary<string, double> gastos = new Dictionary<string, double>();
+
+            Console.WriteLine("digite el nombre del producto : ");
+            string nombreGasto = Console.ReadLine() ?? "";
+
+            while (nombreGasto != "*")
+            {
+                Console.WriteLine("Digite el valor del producto : ");
+                double gasto = Convert.ToDouble(Console.ReadLine());
+
+                gastos.Add(nombreGasto, gasto);
+
+                Console.WriteLine("digite el nombre del producto o (*) para salir : ");
+                nombreGasto = Console.ReadLine() ?? "";
+
+            }
+            
+
+            foreach (var producto in gastos)
+            {
+                Console.WriteLine(producto);
+            }
+
+            double maximo = gastos.Values.Max();
+            double minimo = gastos.Values.Min();
+            double promedio = gastos.Values.Average();
+            double suma = gastos.Values.Sum();
+
+            Console.WriteLine("****************************************************");
+            Console.WriteLine($"Total:  {suma}");
+            Console.WriteLine($"Máximo: {maximo}");
+            Console.WriteLine($"Mínimo:  {minimo}");
+            Console.WriteLine($"Promedio: {promedio:F2}");
+
+            
+        }
+    }
+}
